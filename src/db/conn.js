16efbe06ef24232e436efb59683a265baf47ef67
assis-guilherme <assis.guilherme021@gmail.com>
2023-08-08 08:29:38 -0300

    a

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('toughts', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize('railway', 'root', 'wT68LIzCfnCThEgmaBDi', {
+  host: 'containers-us-west-106.railway.app',
+  port: 6976,
   dialect: 'mysql'
 })
 
